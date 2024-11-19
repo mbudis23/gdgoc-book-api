64411beee4886d030657cd033c14e32b241afe6c
mbudis23 <muhammadbudisetiawan@mail.ugm.ac.id>
2024-11-19 08:12:31 +0700

    Feat: Morgan request logger middleware

--- src/server.js
+++ src/server.js
@@ -1,14 +1,15 @@
-// Lokasi: /app.js
-
 const express = require('express');
 const app = express();
 const bookRoutes = require('./routes/bookRoutes');
 const dotenv = require('dotenv')
+const morgan = require('morgan');
 
 dotenv.config()
 
 app.use(express.json());
+app.use(morgan('dev'))
 app.use('/api/books', bookRoutes);
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
